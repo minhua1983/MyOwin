628fba19667a19fa74b19ce08f52b0352334d092
minh <minh@vosung.cn>
2017-12-04 20:03:59 +0800

    add container

---
+++ MyOwin.Core/Page/Article.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyOwin.Core.Page
+{
+    public class Article
+    {
+        public void List()
+        {
+
+        }
+    }
+}
